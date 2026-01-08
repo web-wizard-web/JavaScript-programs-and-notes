54f06f75e78fc9d453f33b871e813a5386e30692
Gayatri Butkar <gayatributkar14@gmail.com>
2026-01-08 10:40:38 +0530

    Adv js

---
+++ Advanced-JS/explore1.js
@@ -0,0 +1,19 @@
+// asynchronus nature of js - having a pause in between
+// such as during network requests, file reading, or timers
+console.log("Start")
+
+function sayHello()
+{
+    console.log("Say Hello")
+}
+
+setTimeout(() => {
+    sayHello()
+}, 4000); //2seconds
+
+for (i = 0;i<= 10;i++)
+{
+    console.log(i)
+}
+
+// Event Loop 
